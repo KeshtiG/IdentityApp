0b20c9e857ff20296a7f2643a9a00a6140bbf02e
Keshti Gyllinger <gyllinger@gmail.com>
2025-06-05 13:44:07 +0200

    Added some stuff

---
+++ IdentityDemo.Infrastructure/Services/IdentityUserService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityDemo.Application.Dtos;
+using IdentityDemo.Application.Users;
+using IdentityDemo.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityDemo.Infrastructure.Services
+{
+    public class IdentityUserService // Infrastructure-servicen
+    (
+    UserManager<ApplicationUser> userManager, // Hanterar användare
+    SignInManager<ApplicationUser> signInManager, // Hanterar inlogging
+    RoleManager<IdentityRole> roleManager // Hanterar roller
+    ) : IIdentityUserService
+    {
+        public async Task<UserResultDto> CreateUserAsync(UserProfileDto user, string password)
+        {
+            var result = await userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = user.Email,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName
+            }, password);
+            return new UserResultDto(result.Errors.FirstOrDefault()?.Description);
+        }
+
+        public async Task<UserResultDto> SignInAsync(string email, string password)
+        {
+            // Logga in
+            SignInResult result = await signInManager.PasswordSignInAsync(
+            email,
+            password,
+            isPersistent: false,
+            lockoutOnFailure: false);
+            bool wasUserSignedIn = result.Succeeded;
+            // Logga ut
+            //await signInManager.SignOutAsync();
+
+            return new UserResultDto(result.Succeeded ? null : "Invalid user credentials");
+        }
+    }
+}
--- IdentityDemo.Web/Program.cs
+++ IdentityDemo.Web/Program.cs
@@ -1,5 +1,7 @@
 ﻿using IdentityDemo.Application.Users;
 using IdentityDemo.Infrastructure.Persistence;
+using IdentityDemo.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityDemo.Web;
@@ -11,12 +13,35 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllersWithViews();
         builder.Services.AddTransient<IUserService, UserService>();
+        builder.Services.AddTransient<IIdentityUserService, IdentityUserService>();
 
         // Konfigurera EF
         builder.Services.AddDbContext<ApplicationContext>(
             o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+
+        // Identity: Registera identity-klasserna och vilken DbContext som ska användas
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            // Här kan vi (om vi vill) ange inställningar för t.ex. lösenord
+            // (ofta struntar man i detta och kör på default-värdena)
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = true;
+        })
+        .AddEntityFrameworkStores<ApplicationContext>()
+        .AddDefaultTokenProviders();
+        // Identity: Hit ska icke inloggade användare skikas (om de besöker skyddade sidor)
+        builder.Services.ConfigureApplicationCookie(
+         o => o.LoginPath = "/login");
+
+
+
+
         var app = builder.Build();
+
+        // Identity: Behörighet
+        app.UseAuthorization();
 
         app.UseHttpsRedirection();
         app.MapControllers();
